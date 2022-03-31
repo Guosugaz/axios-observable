84003c672da8fe3cea737cb86bb42606754288b0
guojiankun <guojiankun@egridcloud.com>
2022-03-31 16:01:51 +0800

    move componlete to finally

--- lib/create-observable.ts
+++ lib/create-observable.ts
@@ -1,36 +1,48 @@
-import {Observable} from 'rxjs';
-import axios, {AxiosPromise, AxiosRequestConfig, CancelTokenSource} from 'axios';
-import {AxiosObservable} from './axios-observable.interface';
+import { Observable } from "rxjs";
+import axios, {
+  AxiosPromise,
+  AxiosRequestConfig,
+  CancelTokenSource
+} from "axios";
+import { AxiosObservable } from "./axios-observable.interface";
 
-export function createObservable<T>(promiseFactory: (...args: any[]) => AxiosPromise<T>, ...args: any[]): AxiosObservable<T> {
+export function createObservable<T>(
+  promiseFactory: (...args: any[]) => AxiosPromise<T>,
+  ...args: any[]
+): AxiosObservable<T> {
   let config: AxiosRequestConfig = args[args.length - 1];
-  config = config ? {...config} : {};
+  config = config ? { ...config } : {};
   args[args.length - 1] = config;
 
   let cancelSource: CancelTokenSource;
   const hasCancelToken: boolean = !!config.cancelToken;
   if (hasCancelToken) {
-    console.warn(`No need to use cancel token, just unsubscribe the subscription would cancel the http request automatically`);
+    console.warn(
+      `No need to use cancel token, just unsubscribe the subscription would cancel the http request automatically`
+    );
   }
 
   const observable: AxiosObservable<T> = new Observable((subscriber: any) => {
-
     if (!hasCancelToken) {
       cancelSource = axios.CancelToken.source();
       config.cancelToken = cancelSource.token;
     }
 
-    promiseFactory(...args).then(response => {
-      subscriber.next(response);
-      subscriber.complete();
-    })
-        .catch(error => subscriber.error(error));
+    promiseFactory(...args)
+      .then((response) => {
+        subscriber.next(response);
+      })
+      .catch((error) => {
+        subscriber.error(error);
+      })
+      .finally(() => {
+        subscriber.complete();
+      });
   });
 
   const _subscribe = observable.subscribe.bind(observable);
 
   observable.subscribe = (...args2: any[]) => {
-
     const subscription = _subscribe(...args2);
 
     const _unsubscribe = subscription.unsubscribe.bind(subscription);
@@ -45,5 +57,4 @@ export function createObservable<T>(promiseFactory: (...args: any[]) => AxiosPro
   };
 
   return observable;
-
 }
